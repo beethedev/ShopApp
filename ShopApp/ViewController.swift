b8ccb5f08ed4ab13c5f0c95c314a5f402eda542d
Busayo O <44685320+beethedev@users.noreply.github.com>
2021-07-10 16:04:48 -0700

    completed app
    updated challenge solutions for milestones of projects 4-6

--- ShopApp/ViewController.swift
+++ ShopApp/ViewController.swift
@@ -7,11 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Shopping List"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addProduct))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startNewList))
+        
+    }
+
+
+    
+    @objc func startNewList() {
+        shoppingList = [String]()
+        tableView.reloadData()
+        
+    }
+    
+    @objc func addProduct() {
+        let ac = UIAlertController(title: "Add New Item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default){_ in
+            guard let answer = ac.textFields?[0].text else {  return }
+            self.submit(answer) // handler is replaced by closuree
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        shoppingList.insert(lowerAnswer, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        print(shoppingList)
+        
     }
 
 
